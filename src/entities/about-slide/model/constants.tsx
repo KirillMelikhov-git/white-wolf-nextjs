eff7da8dea9f5b14e8351349b5fead4987a9c991
KirillMelikhov-git <kirillka.m220@gmail.com>
2025-12-16 17:33:34 +0300

    fix slider

--- src/entities/about-slide/model/constants.tsx
+++ src/entities/about-slide/model/constants.tsx
@@ -1,4 +1,4 @@
-import slide1 from '../assets/slide-1.jpg';
+import slide1 from '../../../../public/images/background.png';
 import slide2 from '../assets/slide-2.jpg';
 import slide3 from '../assets/slide-3.jpg';
 import slide4 from '../assets/slide-4.jpg';
--- src/entities/dock-card/model/constants.tsx
+++ src/entities/dock-card/model/constants.tsx
@@ -6,7 +6,7 @@ export const dockCards: ICard[] = [
     id: 'agreement',
     image: Loyalty,
     title: 'Договор на оказание платных услуг',
-    link: '/',
+    link: 'https://docs.google.com/document/d/18Q5MHFUCDW_5eItbNn416HRhzCzw5LTT/edit',
   },
   {
     id: 'personal-data',
@@ -18,6 +18,6 @@ export const dockCards: ICard[] = [
     id: 'rules',
     image: Loyalty,
     title: 'Правила внутреннего распорядка',
-    link: '/',
+    link: 'https://docs.google.com/document/d/1BNy6QB7QnfI7PUtCXjX3i-SOU_MdAu2d/edit?usp=sharing&ouid=101541072731352233679&rtpof=true&sd=true',
   },
 ];
