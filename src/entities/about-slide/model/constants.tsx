9fab1d62ced8541d121025d4bd5af512e7eec953
KirillMelikhov-git <kirillka.m220@gmail.com>
2025-12-16 15:34:21 +0300

    feat: add image in slider

--- src/entities/about-slide/model/constants.tsx
+++ src/entities/about-slide/model/constants.tsx
@@ -1,3 +1,4 @@
+import slide1 from '../assets/slide-1.jpg';
 import slide2 from '../assets/slide-2.jpg';
 import slide3 from '../assets/slide-3.jpg';
 import slide4 from '../assets/slide-4.jpg';
@@ -13,7 +14,7 @@ export const aboutSliders: AboutSlide[] = [
     title: 'О нас',
     description:
       'Наша ветеринарная клиника оказывает широкий спектр услуг по лечению домашних животных.',
-    image: '/images/background.png',
+    image: slide1.src,
   },
   {
     id: 'about-2',
