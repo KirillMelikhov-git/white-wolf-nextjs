223725fbc50caafeba3dd82f151f30744d78cb43
Kirill Melikhov <kirillka.m220@gmail.com>
2026-08-17 18:27:44 +0300

    delete notification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function HomePage() {
       </main>
       <Footer />
       <VacancyNotification />
-      <TechnicalNotification />
+      {/* <TechnicalNotification /> */}
     </>
   );
 }
